380c1651c05aeebf121c05ed10b4b7115c015cdf
Stephane MARTINS <stmartin@e1r11p21.42.fr>
2016-04-18 14:32:03 +0200

    add buff 100

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: stmartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/26 10:06:19 by stmartin          #+#    #+#             */
-/*   Updated: 2016/04/08 23:22:38 by stmartin         ###   ########.fr       */
+/*   Updated: 2016/04/18 14:31:31 by stmartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <string.h>
 # include <stdlib.h>
 
-# define BUFF_SIZE 3
+# define BUFF_SIZE 100
 
 typedef struct		s_line
 {
@@ -94,7 +94,7 @@ t_list				*ft_lstnew(void const *content, size_t content_size);
 void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void				ft_putendl(const char *s);
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
-void				ft_lstadd(t_list **alst, t_list *new);
+void				ft_lstadd(t_list **alst, t_list *new_m);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 void				ft_strrev(char *str);
