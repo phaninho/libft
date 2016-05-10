180627cbc80f1debfdd19d6d35e50150674d80e4
Stephane MARTINS <stmartin@e1r10p20.42.fr>
2016-05-10 19:08:06 +0200

    update lstdel

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: stmartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/11 13:25:29 by stmartin          #+#    #+#             */
-/*   Updated: 2016/01/13 16:31:06 by stmartin         ###   ########.fr       */
+/*   Updated: 2016/05/10 19:00:59 by stmartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
 	t_list	*tmp;
 
-	if (!(alst || del))
-		return ;
 	while (*alst)
 	{
 		tmp = (*alst)->next;
